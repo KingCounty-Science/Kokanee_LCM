e674d0494b5349e9d830f105bd7247e09d04eead
Beka Stiling <rstiling@kingcounty.gov>
2024-05-13 17:04:59 -0700

    create graph output for runs

--- R/Kok_LCM_sandbox.R
+++ R/Kok_LCM_sandbox.R
@@ -7,11 +7,11 @@
 #lists based on observations
 hat_egg_surv_list <-c(1, 1,1,1,1,.5,.8) #Jim estimate
 percent_female_list <-c(0.29, 0.28, 0.49, 0.26, 0.39, 0.41, 0.34 ,0.36, 0.30, 0.32, 0.37, 0.34) #spreadsheet
-nat_egg_surv_min <- 0.015 #lower bound of 95% CI
-nat_egg_surv_max <- 0.176 #upper bound of 95% CI
+nat_egg_surv_min <- 0.015 #lower bound of 95% CI #0.015
+nat_egg_surv_max <- 0.176 #upper bound of 95% CI #0.176
 n_nat_eggs_start <- 120000
 n_hat_eggs_start <- 7500
-nat_fry_to_spawn_survival <- .0175 #1.75% geometric mean from jim 2009-2018 
+nat_fry_to_spawn_survival <- .0175 #.0175 #1.75% geometric mean from jim 2009-2018 
 hat_fry_to_spawn_survival <- .0005 #0.05% geometric mean from jim 2009-2018 
 # nat_brood_to_spawn_2 <- c(.04, 0.03, 0.05) prep to build randomness in the future
 portion_nat_brood_to_spawn_age <- c(.0434, .7405, .2123, .0038) #average values for a composition that sums to 1, to estimate the percent of brood that later returned as spawners from 2009-2018
@@ -26,17 +26,21 @@ fecundity_threshold = 1000 #if over this number, then average fecundity is lower
 high_fecundity = 1200
 low_fecundity = 900
 
-years = 100 #how many years would we like to run the model
-runs = 2 # a run is going through the cycle for the number of years desired. how many runs do we do
+years = 150 #how many years would we like to run the model
+runs = 50 # a run is going through the cycle for the number of years desired. how many runs do we do Goal: 1000
 
+grand_df <- matrix(data = NA, nrow = years, ncol = runs+1)
+grand_df[,1] <- 1:years
+
+for(j in 1:runs) {
 ## Create an empty matrix for filling in a loop 
 #natural fish df
-nat_df <- matrix(data = NA, nrow = years, ncol = 7)
-colnames(nat_df) <- c("year","2yo", "3yo","4yo","5yo", "sum_spawn", "eggs")
+nat_df <- matrix(data = NA, nrow = years, ncol = 8)
+colnames(nat_df) <- c("year","2yo", "3yo","4yo","5yo", "sum_spawn_rets", "sum_spawn", "eggs")
 
 #hatchery fish df
-hat_df <- matrix(data = NA, nrow = years, ncol = 7)
-colnames(hat_df) <- c("year", "2yo", "3yo","4yo","5yo", "sum_spawn", "eggs")
+hat_df <- matrix(data = NA, nrow = years, ncol = 8)
+colnames(hat_df) <- c("year", "2yo", "3yo","4yo","5yo", "sum_spawn_rets", "sum_spawn", "eggs")
 
 for(i in 1:years-4) #because outputs are placed 2-5 years into the future, the loop needs to end 4 years prior to the last year/row of the matrix to avoid errors.
 {
@@ -69,7 +73,7 @@ for(i in 1:years-4) #because outputs are placed 2-5 years into the future, the l
   nat_df[i+2,"3yo"] <- n_nat_year_3_spawners
   nat_df[i+3,"4yo"] <- n_nat_year_4_spawners
   nat_df[i+4,"5yo"] <- n_nat_year_5_spawners
-  nat_df[i,"sum_spawn"] <- sum(nat_df[i,2:5], na.rm = TRUE)
+  nat_df[i,"sum_spawn_rets"] <- sum(nat_df[i,2:5], na.rm = TRUE)
 
   ## hatchery fish ####
   n_hat_spawner <- n_hat_fry*hat_fry_to_spawn_survival
@@ -84,20 +88,20 @@ for(i in 1:years-4) #because outputs are placed 2-5 years into the future, the l
   hat_df[i+2,"3yo"] <- n_hat_year_3_spawners
   hat_df[i+3,"4yo"] <- n_hat_year_4_spawners
   hat_df[i+4,"5yo"] <- n_hat_year_5_spawners
-  hat_df[i,"sum_spawn"] <- sum(hat_df[i,2:5], na.rm = TRUE)
+  hat_df[i,"sum_spawn_rets"] <- sum(hat_df[i,2:5], na.rm = TRUE)
   
   if(i < 5) { 
     next } # this tells the code to go back to the top to the next iteration, don't do any more of the script. It only does this for iterations 1-4 while getting the first row full population of spawners established. Otherwise it carries on with the rest.
   else 
     {
   #total the spawners for the next round
-  nat_spawners <- nat_df[i,"sum_spawn"]
+  nat_spawner_rets <- nat_df[i,"sum_spawn_rets"]
 
   #total the spawners for the next round
-  hat_spawners <- hat_df[i,"sum_spawn"]
+  hat_spawner_rets <- hat_df[i,"sum_spawn_rets"]
   
   # spawner to egg ####
-  total_spawners <- nat_spawners + hat_spawners
+  total_spawners <- nat_spawner_rets + hat_spawner_rets
   
   # spawner loss to disease 
   healthy_spawners = total_spawners*(1-loss_to_disease)
@@ -109,6 +113,8 @@ for(i in 1:years-4) #because outputs are placed 2-5 years into the future, the l
   ## spawners -split into portions to use later ####
   hat_spawners <- portion_spawner_to_hatch * healthy_spawners 
   nat_spawners <- healthy_spawners - hat_spawners
+  hat_df[i,"sum_spawn"] <- hat_spawners
+  nat_df[i,"sum_spawn"] <- nat_spawners
   
   #percent female and fecundity
   percent_female <- sample(x = percent_female_list, size = 1, replace = TRUE)
@@ -132,15 +138,50 @@ for(i in 1:years-4) #because outputs are placed 2-5 years into the future, the l
   n_hat_eggs <- n_hat_eggs_1
   
     }
+  
+
+  grand_df[i, j+1] <- healthy_spawners
+
 }
-    
-# Plot #####
-par(mfrow=c(2,2)) # Change the panel layout to 2 x 2
-plot(x = nat_df[5:95,1], y = nat_df[5:95,"sum_spawn"], xlab = "years", ylab = "natural spawners")
-plot(x = hat_df[5:95,1], y = hat_df[5:95,"sum_spawn"], xlab = "years", ylab = "hatchery spawners")
-plot(x = (nat_df[-(1:4),"year"])-4, y = nat_df[-(1:4),"eggs"], xlab = "years", ylab = "natural eggs")
-plot(x = (hat_df[-(1:4),"year"])-4, y = hat_df[-(1:4),"eggs"], xlab = "years", ylab = "hatchery eggs")
-par(mfrow=c(1,1)) # Change back to 1 x 1
+
+if (j == 1) {
+  plot(x = grand_df[5:(years-4),1], 
+       y = grand_df[5:(years-4),j+1], type = "l",
+       xlab = "years", 
+       ylab = "healthy spawners",
+       ylim = c(0, 1000),
+       col = "gray") 
+} else { 
+  lines(x = grand_df[5:(years-4),1],
+        y = grand_df[5:(years-4),1+j],
+        col= "gray")
+} 
+
+
+}  
+
+lines(x = grand_df[5:(years-4),1],
+      y = rowMeans(grand_df[,-1], na.rm = TRUE)[5:(years-4)])
+
+
+#grand_df[i, j] 
+# plot(x = grand_df[5:(years-4),1], 
+#      y = grand_df[5:(years-4),2], type = "l") 
+# lines(grand_df[5:(years-4),1],
+#       y = grand_df[5:(years-4),3])
+# lines(grand_df[5:(years-4),1],
+#       y = grand_df[5:(years-4),4])
+
+
+
+# 
+# # Plot last iteration #####
+# par(mfrow=c(2,2)) # Change the panel layout to 2 x 2
+# plot(x = nat_df[5:(years-5),1], y = nat_df[5:(years-5),"sum_spawn"], xlab = "years", ylab = "natural spawners")
+# plot(x = hat_df[5:(years-5),1], y = hat_df[5:(years-5),"sum_spawn"], xlab = "years", ylab = "hatchery spawners")
+# plot(x = (nat_df[-(1:4),"year"])-4, y = nat_df[-(1:4),"eggs"], xlab = "years", ylab = "natural eggs")
+# plot(x = (hat_df[-(1:4),"year"])-4, y = hat_df[-(1:4),"eggs"], xlab = "years", ylab = "hatchery eggs")
+# par(mfrow=c(1,1)) # Change back to 1 x 1
 
 round(nat_df)
 round(hat_df)
